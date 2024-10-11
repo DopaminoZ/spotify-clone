b063479dad4065b304ba332f5d064be4268c96de
ali-elbagoury <ali.bagoury@gmail.com>
2024-10-11 22:59:33 +0200

    buttons

---
+++ src/components/Header-signup.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Headersignup() {
+  return (
+    <div>Header-signup</div>
+  )
+}
+
+export default Header-signup
--- src/components/sform.js
+++ src/components/sform.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './sform.css'
+import fb from '../images/fb.png'
 
 function sform() {
   return (
     <div class ="buttons">
-     <div class="socials"><button>ddd</button></div> 
-     <div class="socials"><button>ddd</button></div>
-     <div class="socials"><button>ddd</button></div>
+     <div><button class="socials" style={{backgroundColor: "#1877F2"}}><img src={fb}/>ddd</button></div> 
+     <div><button class="socials">ddd</button></div>
+     <div><button class="socials">ddd</button></div>
     </div>
   )
 }
