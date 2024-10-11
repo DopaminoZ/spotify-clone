e53db172b3ed82412a0f560f929eb1a396984896
svare <mon3mpk@gmail.com>
2024-10-11 23:44:23 +0300

    buttons

--- src/components/sform.js
+++ src/components/sform.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './sform.css'
-
+import google from '../images/google.png'
+import fb from'../images/fb.png'
+import apple from '../images/apple.png'
 function sform() {
   return (
     <div class ="buttons">
-     <div class="socials"><button>ddd</button></div> 
-     <div class="socials"><button>ddd</button></div>
-     <div class="socials"><button>ddd</button></div>
+     <div class="socials"><button id='b1'><img src={fb}/>Continue with facebook</button></div> 
+     <div class="socials"><button id='b2'><img src={apple}/>continue with apple</button></div>
+     <div class="socials"><button id='b3'> <img src={google}/> Continue with Google</button></div>
     </div>
   )
 }
