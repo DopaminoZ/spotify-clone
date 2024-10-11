d78248921d80669e6110bbc5dcbcfe2e6bbc6b68
svare <mon3mpk@gmail.com>
2024-10-11 22:33:12 +0300

    buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './components/Header'
+import Sform from'./components/sform'
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header/> 
+      <Sform/>
     </div>
   );
 }
---
+++ src/components/sform.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './sform.css'
+
+function sform() {
+  return (
+    <div class ="buttons">
+     <div class="socials"><button>ddd</button></div> 
+     <div class="socials"><button>ddd</button></div>
+     <div class="socials"><button>ddd</button></div>
+    </div>
+  )
+}
+
+export default sform
