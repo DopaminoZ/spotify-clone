4c59b117195fc29b56873afb759920c406b4fbad
ali-elbagoury <110342114+DopaminoZ@users.noreply.github.com>
2024-10-26 01:50:16 +0300

    kamelo

--- src/components/js/Artists.js
+++ src/components/js/Artists.js
@@ -1,26 +1,17 @@
 import React from 'react'
-import Header from "./Header_HomePage"
 import styles from "../css/Artists.module.css"
-import LeftSideNav from './LeftSideNav'
 import yeat from '../../assets/images/yeat.jpg'
-import containerstyle from '../css/BrowseComponent.module.css'
 
 function Artists() {
   return (
-    <div className={containerstyle.container}>
-      
-  
-      
-
-
-  
-          <div className={styles.art}>
+    <div id={styles.container} className={styles.art}>
             <div className={styles.profile}>
-                <img src={yeat} className={styles.image}></img></div>
+                <img src={yeat} className={styles.image}></img>
                 <h1 className={styles.name}>Yeat</h1>
+            </div>
+            <div id={styles.songlist}>
 
-          </div>
-
+            </div>
     </div>
   )
 }
