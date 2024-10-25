52bdb7b4384d474e4477918a36db77a7faf66edf
ali-elbagoury <ali.bagoury@gmail.com>
2024-10-25 20:36:45 +0200

    Incomplete playlist/album component

--- src/components/js/HomePage.jsx
+++ src/components/js/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "../css/HomePage.module.css";
 import Header_HomePage from "./Header_HomePage";
 import LeftSideNav from "./LeftSideNav";
 import BrowseComponent from "./BrowseComponent";
-import styles from "../css/HomePage.module.css";
 import MainpageComponent from "./MainpageComponent";
+import Playlist from "./Playlist";
 function HomePage() {
   return (
     <div id={styles.mainpage}>
---
+++ src/components/js/Playlist.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from "../css/Playlist.module.css"
+import containerstyle from "../css/BrowseComponent.module.css"
+import hanz from "../../assets/images/elbasha.png"
+function Playlist() {
+  return (
+    <div id={containerstyle.container} className={styles.gradient}>
+        <div id={styles.uppercontainer}>
+        <img id={styles.playlistimg} src={hanz}></img>
+        <div id={styles.playlistdetails}>
+            <p id={styles.playlisttype}></p>
+            <h1 id={styles.albumtitle}></h1>
+            <div id={styles.artistdiv}>
+            <img id={styles.artistimage}></img> <p id={styles.artistname}></p> <p>•</p> <p id={styles.year}>year</p> <p>•</p> <p id={styles.details}>details</p>
+            </div>
+        </div>
+        </div>
+        <div id={styles.midcontainer}>
+            
+        </div>
+        <div id={styles.playlistdiv}>
+
+        </div>
+    </div>
+  )
+}
+
+export default Playlist
