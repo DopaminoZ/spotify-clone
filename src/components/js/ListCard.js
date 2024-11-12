66e5371b1ba43ab1646563c1548b1fd0d3cf196f
ali-elbagoury <110342114+DopaminoZ@users.noreply.github.com>
2024-11-12 14:27:17 +0200

    playbutton

--- src/components/js/ListCard.js
+++ src/components/js/ListCard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from "../css/ListCard.module.css"
+import play from "../../assets/images/play.png"
 function ListCard({album}) {
   return (
     <div id={styles.container}>
         <img id={styles.image} src={album.image}></img>
         <p id={styles.description}>{album.title} - {album.artist}</p>
+        <button id={styles.playbutton}><img src={play} /></button>
     </div>
   )
 }
