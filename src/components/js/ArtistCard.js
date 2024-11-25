529b54636a32496796e2ba599a7fc3d210cd3f1c
svare <mon3mpk@gmail.com>
2024-11-26 00:13:33 +0200

    ArtistPage

---
+++ src/components/js/ArtistCard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from "../css/ArtistCard.module.css"
+import photo from "../../assets/images/muhab.jpg"
+
+
+
+const ArtistCard = () => {
+  return (
+    <div className={styles.container}>
+      
+        
+        
+
+    <h1 className={styles.num}>1</h1>
+    <img src={photo} id={styles.pic}></img>
+    <h1 className={styles.name}>Illmond</h1>
+    <h1 className={styles.lnum}>1,356,779</h1>
+    <h1 className={styles.time}>3:56</h1>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default ArtistCard
--- src/components/js/Artists.js
+++ src/components/js/Artists.js
@@ -1,17 +1,56 @@
 import React from 'react'
 import styles from "../css/Artists.module.css"
-import yeat from '../../assets/images/yeat.jpg'
+import badge from "../../assets/images/badge.webp"
+import play from "../../assets/images/play.png"
+import dots from "../../assets/images/dots.webp"
+import Card from "./ArtistCard"
+
 
 function Artists() {
   return (
     <div id={styles.container} className={styles.art}>
             <div className={styles.profile}>
-                <img src={yeat} className={styles.image}></img>
-                <h1 className={styles.name}>Yeat</h1>
-            </div>
-            <div id={styles.songlist}>
+
+
+                <div className={styles.verify}>
+                <img id={styles.icon} src={badge}></img>
+                <h3 className={styles.vtext}>Verified Artist</h3>
+                </div>    
+
+                <h1 className={styles.name}>Muhab</h1>
+
+                <h1 className={styles.monthly}>500,467 monthly listeners</h1>
+
 
             </div>
+            <div id={styles.container2}>
+
+              <div className={styles.buttons}>
+                 <button class={styles.btn1} >
+                     <img id ={styles.btn}src={play}></img>
+                  </button>
+                  <button className={styles.follow}>Follow</button>
+                  <button className={styles.dotbtn}> <img class ={styles.dot}src={dots}></img></button>
+                  <h1 className={styles.popular}>Popular</h1>
+
+              </div> 
+
+              
+              <h1 className={styles.popular}>Popular</h1>
+              <Card/>
+              <Card/>
+              <Card/>
+              <Card/>
+              <Card/>
+              <Card/>
+              
+              
+              
+
+          
+
+            </div>
+           
     </div>
   )
 }
