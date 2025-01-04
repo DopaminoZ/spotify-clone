3ee3f4f6f9b9a44016b2412294e8960595448c08
svare <mon3mpk@gmail.com>
2025-01-05 00:50:21 +0200

    profile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import LogInPage from "./components/js/LogInPage.js";
 import SignUpPage from "./components/js/SignUp.js";
 import HomePage from "./components/js/HomePage.jsx";
+import Profile from './components/js/profile.js'
 import 'simplebar'; // Import the library
 import 'simplebar/dist/simplebar.css'; // Import the default CSS styles
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
@@ -16,6 +17,9 @@ function App() {
           </Route>
           <Route path="/login">
             <LogInPage />
+          </Route>
+          <Route path="/profile">
+            <Profile />
           </Route>
           <Route path="/">
             <HomePage />
